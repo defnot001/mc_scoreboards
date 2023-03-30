c3ba7be4ca3397d1706cf80016e8f584a6d18729
defnot001 <marc.miller994@gmail.com>
2023-03-30 20:25:23 +0200

    changed to using const

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ use clap::Parser;
 use cli::Cli;
 use datapack::generate_datapack;
 
+const SUPPORTED_VERSIONS: &[&str] = &["1.16.5", "1.17.1", "1.18.2", "1.19.2", "1.19.3", "1.19.4"];
+
 fn main() {
     let cli = Cli::parse();
 
@@ -14,18 +16,17 @@ fn main() {
         panic!("Unsupported version: {}", &cli.game_version);
     }
 
-    println!("Generating datapack for Minecraft {}...", &cli.game_version);
-
     let scoreboards = scoreboards::get_scoreboards_from_version(&cli.game_version)
         .expect("Could not get scoreboards");
 
-    println!("Generating {} scoreboard functions", scoreboards.len() * 2);
-
     generate_datapack(scoreboards, &cli).expect("Could not generate datapack");
+
+    println!(
+        "Successfully generated datapack for Minecraft {}",
+        &cli.game_version
+    );
 }
 
 fn check_version(version: &str) -> bool {
-    let supported_versions = vec!["1.16.5", "1.17.1", "1.18.2", "1.19.2", "1.19.3", "1.19.4"];
-
-    supported_versions.contains(&version)
+    SUPPORTED_VERSIONS.contains(&version)
 }
