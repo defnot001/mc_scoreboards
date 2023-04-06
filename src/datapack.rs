bb3cf2c31d7272e20d5e494189880fd70b96b70a
defnot001 <marc.miller994@gmail.com>
2023-04-06 16:15:17 +0200

    feat: generate update.mcfunction

--- src/datapack.rs
+++ src/datapack.rs
@@ -1,42 +1,138 @@
+use std::collections::HashMap;
+use std::error::Error;
 use std::fs::{self, File};
-use std::io::{Error, Write};
+use std::io::{self, Error as IOError, ErrorKind, Write};
 use std::path::{Path, PathBuf};
 
 use crate::cli::Cli;
 use crate::scoreboards::MCScoreboard;
+use crate::stats::{get_stats, PlayerStats};
 
-pub fn generate_datapack(scoreboards: Vec<MCScoreboard>, options: &Cli) -> Result<(), Error> {
-    let datapack_path = create_datapack_directory(&options)?;
-    let datapack_function_path = create_function_directory(&datapack_path)?;
+#[derive(Debug)]
+struct DatapackPaths {
+    base_path: PathBuf,
+    function_path: PathBuf,
+}
 
-    create_mcmeta_file(&datapack_path, &options.game_version)?;
-    write_create_remove_functions(scoreboards, &datapack_function_path)?;
+struct MCStat {
+    player_name: String,
+    stat_type: String,
+    item: String,
+    score: u32,
+}
+
+impl DatapackPaths {
+    pub fn new(custom_path: &Path, version: &str) -> Self {
+        let base_path = Path::join(custom_path, format!("datapacks/mc-scoreboards-{}", version));
+        let function_path = Path::join(&base_path, "data/mc-scoreboards/functions");
+
+        DatapackPaths {
+            base_path,
+            function_path,
+        }
+    }
+}
+
+pub fn generate_datapack(
+    scoreboards: Vec<MCScoreboard>,
+    options: &Cli,
+) -> Result<(), Box<dyn Error>> {
+    let paths = DatapackPaths::new(&options.output_dir, &options.game_version);
+
+    create_datapack_structure(&paths.function_path)?;
+    create_mcmeta_file(&paths.base_path, &options.game_version)?;
+    create_base_functions(scoreboards, &paths.function_path)?;
+
+    if let Some(stats_path) = &options.stats_dir {
+        let Some(whitelist_path) = &options.whitelist_path else {
+            return Err(Box::new(IOError::new(
+                ErrorKind::NotFound,
+                "Missing whitelist file argument. Please provide a whitelist file with the --whitelist (-w) argument.",
+            )));
+        };
+
+        let stats = get_stats(whitelist_path, stats_path)?;
+
+        let mut update_file = File::create(Path::join(&paths.function_path, "update.mcfunction"))?;
+
+        for player_stats in stats {
+            let mcstats = convert_stats(player_stats);
+
+            for stat in mcstats {
+                writeln!(
+                    &mut update_file,
+                    "scoreboard players set {} {} {} {}",
+                    stat.player_name, stat.stat_type, stat.item, stat.score
+                )?;
+            }
+        }
+    }
 
     Ok(())
 }
 
-fn create_datapack_directory(options: &Cli) -> Result<PathBuf, Error> {
-    let datapack_path = Path::join(
-        &options.output_dir,
-        format!("datapacks/mc-scoreboards-{}", options.game_version),
-    );
-    fs::create_dir_all(&datapack_path)?;
+fn convert_stats(ps: PlayerStats) -> Vec<MCStat> {
+    let mut all_stats: Vec<MCStat> = Vec::new();
 
-    Ok(datapack_path)
-}
+    let new_stats = [
+        map_stat(&ps.player_name, ps.stats.mined, "m"),
+        map_stat(&ps.player_name, ps.stats.crafted, "c"),
+        map_stat(&ps.player_name, ps.stats.used, "u"),
+        map_stat(&ps.player_name, ps.stats.broken, "b"),
+        map_stat(&ps.player_name, ps.stats.dropped, "d"),
+        map_stat(&ps.player_name, ps.stats.picked_up, "p"),
+        map_stat(&ps.player_name, ps.stats.killed, "k"),
+        map_stat(&ps.player_name, ps.stats.killed_by, "kb"),
+        map_stat(&ps.player_name, ps.stats.custom, "z"),
+    ];
 
-fn create_function_directory(datapack_path: &Path) -> Result<PathBuf, Error> {
-    let datapack_function_path = Path::join(datapack_path, "data/mc-scoreboards/functions");
-    if datapack_function_path.exists() {
-        fs::remove_dir_all(&datapack_function_path)?;
+    for mut map in new_stats {
+        all_stats.append(&mut map);
     }
-    fs::create_dir_all(&datapack_function_path)?;
 
-    Ok(datapack_function_path)
+    all_stats
 }
 
-fn create_mcmeta_file(datapack_path: &Path, version: &str) -> Result<(), Error> {
-    let mut file = File::create(datapack_path.join("pack.mcmeta"))?;
+fn map_stat(player_name: &str, stats: HashMap<String, u32>, short_type: &str) -> Vec<MCStat> {
+    let stats: Vec<MCStat> = stats
+        .iter()
+        .map(|(k, v)| MCStat {
+            player_name: player_name.to_string(),
+            stat_type: format!("{}-", short_type),
+            item: k[10..].to_string(),
+            score: *v,
+        })
+        .collect();
+
+    stats
+}
+
+// pub fn create_update_functions(stats: Vec<Stats>, functions_path: &Path) -> Result<(), Error> {
+//     let mut update_file = File::create(Path::join(&functions_path, "update.mcfunction"))?;
+
+//     for stat in stats {
+//         let mcstats = convert_stats(stat);
+
+//         for stat in mcstats {
+//             println!("scoreboard players set {}")
+//         }
+//     }
+
+//     Ok(())
+// }
+
+fn create_datapack_structure(functions_path: &Path) -> io::Result<()> {
+    if functions_path.exists() {
+        fs::remove_dir_all(functions_path)?;
+    }
+
+    fs::create_dir_all(functions_path)?;
+
+    Ok(())
+}
+
+fn create_mcmeta_file(base_path: &Path, version: &str) -> io::Result<()> {
+    let mut file = File::create(base_path.join("pack.mcmeta"))?;
 
     let content = format!(
         r#"{{
@@ -49,17 +145,14 @@ fn create_mcmeta_file(datapack_path: &Path, version: &str) -> Result<(), Error> 
         version
     );
 
-    file.write_all(&content.as_bytes())?;
+    file.write_all(content.as_bytes())?;
 
     Ok(())
 }
 
-fn write_create_remove_functions(
-    scoreboards: Vec<MCScoreboard>,
-    functions_path: &PathBuf,
-) -> Result<(), Error> {
-    let mut create_file = File::create(Path::join(&functions_path, "create.mcfunction"))?;
-    let mut remove_file = File::create(Path::join(&functions_path, "remove.mcfunction"))?;
+fn create_base_functions(scoreboards: Vec<MCScoreboard>, functions_path: &Path) -> io::Result<()> {
+    let mut create_file = File::create(Path::join(functions_path, "create.mcfunction"))?;
+    let mut remove_file = File::create(Path::join(functions_path, "remove.mcfunction"))?;
 
     for stat in scoreboards {
         if let Some(name) = stat.name {
