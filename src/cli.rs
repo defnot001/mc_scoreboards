fe698a8bb599d07690651c562f2896aa7fcbe1d3
defnot001 <marc.miller994@gmail.com>
2023-04-06 18:07:51 +0200

    changed descriptions

--- src/cli.rs
+++ src/cli.rs
@@ -6,18 +6,18 @@ use clap::Parser;
 #[command(version, author, about, long_about = None)]
 #[command(name = "mc-scoreboards")]
 pub struct Cli {
-    /// The version of the game to generate the datapack for. Supported versions: 1.19.3, 1.19.4
+    /// The version of the game to generate the datapack for. Supported versions: 1.16.5, 1.17.1, 1.18.2, 1.19.2, 1.19.3, 1.19.4
     pub game_version: String,
 
     /// The path to the stats directory. If this is passed, the program will also create an update.mcfunction file.
     #[arg(short = 's', long = "stats")]
     pub stats_dir: Option<PathBuf>,
 
-    /// The directory to output the datapack to. Defaults to the current directory. You can't pass this argument, if you don't pass the stats argument.
+    /// The directory to output the datapack to. Defaults to the current directory.
     #[arg(short = 'o', long = "outdir", default_value = ".")]
     pub output_dir: PathBuf,
 
-    /// The path to the whitelist.json file. If this is passed, the program will only update the stats for whitelisted players, which can significantly reduce the size of the datapack.
+    /// The path to the whitelist.json file. If you pass the stats directory, this is required.
     #[arg(short = 'w', long = "whitelist")]
     pub whitelist_path: Option<PathBuf>,
 }
